ae81ea226ca8ef4a6940d8fc7538f774e1bc6450
Trong <trongnv.bi12-449@st.usth.edu.vn>
2024-10-18 11:54:03 +0700

    Commit

--- training/test_embedding.py
+++ training/test_embedding.py
@@ -1,5 +1,3 @@
-# (phương pháp mới) so sánh dữ liệu với nhãn có cùng chứng từ
-
 import json
 import torch
 import numpy as np
@@ -78,57 +76,57 @@ def predict_label(customer_file):
         json.dump(customer_data, f, ensure_ascii=False, indent=4)
     
 
-def calculate_accuracy(data_file, label_file):
-    with open(data_file, 'r', encoding='utf-8') as f:
-        data = json.load(f)
-    with open(label_file, 'r', encoding='utf-8') as f:
-        label_data = json.load(f)
+# def calculate_accuracy(data_file, label_file):
+#     with open(data_file, 'r', encoding='utf-8') as f:
+#         data = json.load(f)
+#     with open(label_file, 'r', encoding='utf-8') as f:
+#         label_data = json.load(f)
 
-    total_records = 0
-    total_score = 0
-    max_score = 10
+#     total_records = 0
+#     total_score = 0
+#     max_score = 10
 
-    for item in data:
-        customer_label = item.get("Loại nghiệp vụ").strip()
-        first_predict_label = item.get("label top 1").strip()
-        describe = item.get("Diễn giải").strip()
+#     for item in data:
+#         customer_label = item.get("Loại nghiệp vụ").strip()
+#         first_predict_label = item.get("label top 1").strip()
+#         describe = item.get("Diễn giải").strip()
         
-        for label_item in label_data:
-            label_type = label_item["Loại chứng từ chuẩn hoá"].strip()
-            labels = label_item["Nghiệp vụ chi tiết (đầu ra)"].strip()
+#         for label_item in label_data:
+#             label_type = label_item["Loại chứng từ chuẩn hoá"].strip()
+#             labels = label_item["Nghiệp vụ chi tiết (đầu ra)"].strip()
 
-        if customer_label is None or first_predict_label is None or describe is None:
-            continue
+#         if customer_label is None or first_predict_label is None or describe is None:
+#             continue
 
-        total_records += 1
+#         total_records += 1
 
-        labels_processed = [
-            item.get("label top 1")
-            # item.get("label top 2"),
-            # item.get("label top 3"),
-            # item.get("label top 4"),
-            # item.get("label top 5")
-        ]
+#         labels_processed = [
+#             item.get("label top 1")
+#             # item.get("label top 2"),
+#             # item.get("label top 3"),
+#             # item.get("label top 4"),
+#             # item.get("label top 5")
+#         ]
 
-        # weights = [1.0, 1.0, 1.0, 1.0, 1.0]
+#         # weights = [1.0, 1.0, 1.0, 1.0, 1.0]
 
-        match_score = 0
-        for idx, label in enumerate(labels_processed):
-            if label is not None and customer_label.strip().lower() == first_predict_label.strip().lower():
-                # match_score = max_score * weights[idx]
-                match_score = max_score
-                break
+#         match_score = 0
+#         for idx, label in enumerate(labels_processed):
+#             if label is not None and customer_label.strip().lower() == first_predict_label.strip().lower():
+#                 # match_score = max_score * weights[idx]
+#                 match_score = max_score
+#                 break
 
-        total_score += match_score
+#         total_score += match_score
 
-    if total_records > 0:
-        accuracy = (total_score / (total_records * max_score)) * 100
-    else:
-        accuracy = 0.0
+#     if total_records > 0:
+#         accuracy = (total_score / (total_records * max_score)) * 100
+#     else:
+#         accuracy = 0.0
 
-    print("Tên file: ", data_file)
-    print(f"Tổng số bản ghi hợp lệ: {total_records}")
-    print(f"Độ chính xác: {accuracy:.2f}%")
+#     print("Tên file: ", data_file)
+#     print(f"Tổng số bản ghi hợp lệ: {total_records}")
+#     print(f"Độ chính xác: {accuracy:.2f}%")
 
 if __name__ == "__main__":
     customer_file = '2 neg dataset/test.json'
@@ -136,4 +134,4 @@ if __name__ == "__main__":
     output_file = "updated_customer_data.json"
     add_label_file(label_file)
     predict_label(customer_file)
-    calculate_accuracy(output_file, label_file)
+    # calculate_accuracy(output_file, label_file)
